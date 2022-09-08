60ea0713d111b4a443fec1f5d5362950184d5e3d
Zaacky24 <zaacky98@gmail.com>
2022-09-08 13:19:34 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- Prints message
+ * Return: zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,2 @@
 0-preprocessor
+4-puts.c
