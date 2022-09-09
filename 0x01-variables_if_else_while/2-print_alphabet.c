ff7b479a73fcd0c31dbeee03d181d63f53ae127a
Zaacky24 <zaacky98@gmail.com>
2022-09-09 14:24:11 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the program run from here
+ *
+ * Return:return zero if no erro and non zero if error
+*/
+int main(void)
+{
+	int alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0-positive_or_negative.c
 1-last_digit.c
+2-print_alphabet
