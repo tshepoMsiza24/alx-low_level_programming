dd58c4b95082b26012bc5c7d4ff568d09b3c1969
Zaacky24 <zaacky98@gmail.com>
2022-09-20 03:44:06 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,8 +10,12 @@ void print_rev(char *s)
 {
 	int index;
 
+
+	/*finds the length of string without null character*/
 	for (index = 0; s[index] != '\0'; ++index)
 
+		;
+	/*print char from thr last index as you decrement*/
 	for (--index; index >= 0; --index)
 		_putchar(s[index]);
 	_putchar('\n');
