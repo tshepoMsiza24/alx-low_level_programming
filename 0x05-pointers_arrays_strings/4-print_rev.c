1dfda2f87ad6026820aceae1a064a9ba0ccd1fdc
Zaacky24 <zaacky98@gmail.com>
2022-09-20 03:36:48 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; ++index)
+
+	for (--index; index >= 0; --index)
+		_putchar(s[index]);
+	_putchar('\n');
+}
