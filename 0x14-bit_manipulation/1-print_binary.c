c3a720fe447e9829c9a9f4b4b91f66a9c6cf88ec
Zaacky24 <zaacky98@gmail.com>
2022-10-27 14:07:27 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary - function to convert number to binary
+ * @n: unsigned long  int type;
+ * Return: always successful
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int check;
+	unsigned int size = sizeof(n) * 8;
+	unsigned int count;
+	unsigned int flag = 1;
+
+	count = 0;
+	while (count < size)
+	{
+		check = (n << 1);
+		check >>= 1;
+		if (n != check)
+		{
+			flag = 0;
+			write(1, "1", 1);
+		}
+		else if (!flag)
+		{
+			write(1, "0", 1);
+		}
+		n <<=  1;
+		count++;
+	}
+	if (flag == 1)
+		write(1, "0", 1);
+}
