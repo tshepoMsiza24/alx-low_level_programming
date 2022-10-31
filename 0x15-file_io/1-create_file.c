8d303e9d22cc13b888fe95cbc3d053a14e2b2269
Zaacky24 <zaacky98@gmail.com>
2022-10-31 13:05:49 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - function that creates file
+ * @filename: pointer to filename to be created of char const type
+ * @text_content: string to be written into file
+ * Return: always successfull
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, length, fdwrite;
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+	length = 0;
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
+	fdwrite = write(fd, text_content, length);
+	if (fdwrite == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
