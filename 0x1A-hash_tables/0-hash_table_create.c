e8de4b2ede0917bd2dd93c8cf148ab4b2761a231
Zaacky24 <zaacky98@gmail.com>
2023-02-18 14:32:05 -0800

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: number of hash table buckets to create
+ * Return: address of new hash table, or NULL if failure
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	unsigned long int index;
+	/*Handle errors*/
+	if (size == 0)
+		return (NULL);
+
+	/*Allocated memory*/
+	new_table = malloc(sizeof(hash_table_t));
+	/*Handle errors*/
+	if (new_table == NULL)
+		return (NULL);
+
+	/*Allocated memory*/
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (new_table->array == NULL)
+		return (NULL);
+
+	new_table->size = size;
+
+	for (index = 0; index < size; index++)
+		new_table->array[index] = NULL;
+
+	return (new_table);
+}
